870e8120ed14a6946a9651ae5bea2b38f02f0b52
rady osama <radyosama200@gmail.com>
2025-06-27 04:40:51 +0300

    edit in icon & fixed some bugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,7 +86,7 @@
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link active text-danger-emphasis" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                    <i class="fa fa-sign-out fs-5 text-white" aria-hidden="true"></i>
+                                    <i class="fa fa-sign-out fs-5 text-dark" aria-hidden="true"></i>
                                 </a>
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                     @csrf
